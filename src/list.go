32a6c3613b11a26962070be9ec54ef2f94aa0c73
MrSerge01 <86667481+MrSerge01@users.noreply.github.com>
2025-12-07 22:01:17 +0500

    qol pt1

--- src/list.go
+++ src/list.go
@@ -12,45 +12,41 @@ import (
 
 func List(file *os.File, db DB) {
 	app := tview.NewApplication()
-	mainView := tview.NewTreeNode("Welcome to Notabena!").SetColor(tcell.ColorMediumPurple)
-	noteTree := tview.NewTreeView().
-		SetRoot(mainView).
-		SetCurrentNode(mainView)
+	mainView := tview.NewTreeNode("Welcome to Notabena!").SetColor(tcell.ColorMediumPurple).SetSelectable(false)
+	noteTree := tview.NewTreeView().SetRoot(mainView).SetCurrentNode(mainView)
 
 	for _, v := range db.GetNotes() {
 		stringId := strconv.FormatUint(uint64(v.Id), 10)
 		node := tview.NewTreeNode(v.Name + " [grey]#" + stringId + "[white]")
-		node.
-			SetReference(v.Id).
-			SetSelectable(true)
-		node.SetExpanded(false)
+		node.SetReference(v.Id).SetExpanded(false)
 		mainView.AddChild(node)
 		node.AddChild(
-			tview.NewTreeNode("Edit").SetReference("EDT+" + stringId).SetSelectable(true).SetColor(tcell.ColorLightCyan),
+			tview.NewTreeNode("Edit").SetReference("EDT+" + stringId).SetColor(tcell.ColorLightCyan),
 		)
 		node.AddChild(
-			tview.NewTreeNode("View").SetReference("VWR+" + stringId).SetSelectable(true),
+			tview.NewTreeNode("View").SetReference("VWR+" + stringId),
 		)
 		node.AddChild(
-			tview.NewTreeNode("Delete").SetReference("DEL+" + stringId).SetSelectable(true).SetColor(tcell.ColorRed),
+			tview.NewTreeNode("Delete").SetReference("DEL+" + stringId).SetColor(tcell.ColorRed),
 		)
 	}
 
 	mainView.AddChild(
-		tview.NewTreeNode("Create note!").SetReference("NEW").SetSelectable(true).SetColor(tcell.ColorBlue),
+		tview.NewTreeNode("Create a Note!").SetReference("NEW").SetColor(tcell.ColorBlue),
 	)
 	mainView.AddChild(
-		tview.NewTreeNode("(Use Enter + arrow keys to navigate)").SetReference(nil).SetSelectable(false).SetColor(tcell.ColorDarkCyan),
+		tview.NewTreeNode("Exit Notabena!").SetReference("EXT").SetColor(tcell.ColorRed),
 	)
 
 	noteTree.SetSelectedFunc(func(node *tview.TreeNode) {
 		reference := node.GetReference()
-		if reference == nil {
-			return // Selecting the root node does nothing.
-		}
 		if reference == "NEW" {
 			app.Stop()
 			Create(file, db, 0)
+			return
+		}
+		if reference == "EXT" {
+			app.Stop()
 			return
 		}
 		str, ok := reference.(string)
